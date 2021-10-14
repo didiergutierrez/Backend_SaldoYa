240a209c3bec62c4199c08991629b08d584aeb27
didiergutierrez <digegu0532@gmail.com>
2021-10-13 20:26:01 -0500

    v1

---
+++ src/main/java/com/example/Backend_SaldoYa/BackendSaldoYaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_SaldoYa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSaldoYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSaldoYaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Backend_SaldoYa/BackendSaldoYaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_SaldoYa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSaldoYaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
