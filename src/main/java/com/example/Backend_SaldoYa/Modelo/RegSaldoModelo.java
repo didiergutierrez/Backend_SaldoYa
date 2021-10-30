827edab1b8ffacdfecd7025cd0de930868a1b9c5
didiergutierrez <digegu0532@gmail.com>
2021-10-29 23:22:51 -0500

    RegSaldo_&tienda

--- src/main/java/com/example/Backend_SaldoYa/Modelo/RegSaldoModelo.java
+++ src/main/java/com/example/Backend_SaldoYa/Modelo/RegSaldoModelo.java
@@ -1,5 +1,45 @@
 package com.example.Backend_SaldoYa.Modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="regSaldo")
 public class RegSaldoModelo {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    long id;
+    long idTienda;public long getId() {
+    return id;
+}
+public void setId(long id) {
+    this.id = id;
+}
+
+    public long getIdTienda() {
+        return idTienda;
+    }
+    public void setIdTienda(long idTienda) {
+        this.idTienda = idTienda;
+    }
+    public long getCedula() {
+        return cedula;
+    }
+    public void setCedula(long cedula) {
+        this.cedula = cedula;
+    }
+    public int getValorSaldo() {
+        return valorSaldo;
+    }
+    public void setValorSaldo(int valorSaldo) {
+        this.valorSaldo = valorSaldo;
+    }
+    long cedula;
+    int valorSaldo;
+
 }
--- src/main/java/com/example/Backend_SaldoYa/Modelo/TiendaModelo.java
+++ src/main/java/com/example/Backend_SaldoYa/Modelo/TiendaModelo.java
@@ -1,5 +1,45 @@
 package com.example.Backend_SaldoYa.Modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tienda")
 public class TiendaModelo {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    long id;
+    String NombreTienda;
+    String tipoNegocio;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNombreTienda() {
+        return NombreTienda;
+    }
+    public void setNombreTienda(String nombreTienda) {
+        NombreTienda = nombreTienda;
+    }
+    public String getTipoNegocio() {
+        return tipoNegocio;
+    }
+    public void setTipoNegocio(String tipoNegocio) {
+        this.tipoNegocio = tipoNegocio;
+    }
+    public String getContraseña() {
+        return Contraseña;
+    }
+    public void setContraseña(String contraseña) {
+        Contraseña = contraseña;
+    }
+    String Contraseña;
+        
 }
